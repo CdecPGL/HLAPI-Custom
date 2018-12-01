5e5a896c3d19b5e36dc84f13132b151511a05c03
Cdec <cdecrement@gmail.com>
2018-12-02 01:41:33 +0900

    [update] Change assembly info of UNetWeaver

--- Unity-Technologies-networking/Weaver/AssemblyInfo.cs
+++ Unity-Technologies-networking/Weaver/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Unity.UNetWeaver")]
+[assembly: AssemblyTitle("PlanetaGameLabo.UNetWeaver")]
 [assembly: AssemblyDescription("UNET assembly post processor for code generation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Unity Technologies")]
-[assembly: AssemblyProduct("Unity.UNetWeaver")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCompany("PlanetaGameLabo")]
+[assembly: AssemblyProduct("PlanetaGameLabo.UNetWeaver")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Assembly-CSharp-Editor-testable")]
